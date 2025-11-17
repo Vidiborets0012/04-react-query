f5c770284813a67f77bfd9bec26e2bdb0d90071f
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-17 10:58:31 +0200

    install and configure QueryClient and QueryClientProvider in src/main.tsx, add React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "modern-normalize";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+//1. імпортувати та створити новий QueryClient
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  //2. обгорнути додаток в QueryClientProvider, передавши в нього створений клієнт
+  //3. додати React Query Devtools
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
